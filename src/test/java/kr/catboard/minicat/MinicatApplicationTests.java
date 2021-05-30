f6a10ed7a1ca843d0c2abba9d7fae8308b1369d9
reyoucat <re.you.cat@gmail.com>
2021-05-30 15:59:49 +0900

    Chore: 프로젝트 초기 설정

---
+++ src/test/java/kr/catboard/minicat/MinicatApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.catboard.minicat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinicatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

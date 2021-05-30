d94f5003f0077c619050d734932995720b8a14ae
reyoucat <re.you.cat@gmail.com>
2021-05-30 16:34:45 +0900

    Chore: 프로젝트 초기 설정 (#2)

---
+++ src/main/java/kr/catboard/minicat/MinicatApplication.java
@@ -0,0 +1,13 @@
+package kr.catboard.minicat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinicatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinicatApplication.class, args);
+	}
+
+}
